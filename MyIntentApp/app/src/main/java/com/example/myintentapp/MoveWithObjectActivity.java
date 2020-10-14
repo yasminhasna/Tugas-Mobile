cd25177356edda64a36524b07b28bdac45f272cc
yasminhasna <yasmin.hasna16@gmail.com>
2020-10-14 09:13:42 +0700

    Tugas Praktikum

---
+++ MyIntentApp/app/src/main/java/com/example/myintentapp/MoveWithObjectActivity.java
@@ -0,0 +1,20 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithObjectActivity extends AppCompatActivity {
+    public static String EXTRA_PERSON = "extra_person";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_object);
+        TextView tvObject = (TextView) findViewById(R.id.tv_object_received);
+        Person mPerson = getIntent().getParcelableExtra(EXTRA_PERSON);
+        String text = "Name : "+mPerson.getName()+", Email : "+mPerson.getEmail()+", Age : "+mPerson.getAge() + ", Location : "+mPerson.getCity();
+        tvObject.setText(text);
+        }
+}
